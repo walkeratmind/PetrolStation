b247d8364d5d1741db5eaa9245938ac71d978f26
Rakesh Niraula <Rakeshniraula@outlook.com>
2019-04-19 12:49:33 +0545

    add map activity

---
+++ app/src/main/java/com/example/petrolstation/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.petrolstation;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "Main Activity";
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isServicesOk()) {
+            init();
+        }
+    }
+
+    private void init() {
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
+    }
+
+    private boolean isServicesOk() {
+        Log.d(TAG, "isServicesOK: Checking google Services Version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            // Everything is cool and user can request for map
+            Log.d(TAG, "isServicesOK: Google Play Services is working ");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+//            an error is occured but can be fixed
+            Log.d(TAG, "isServicesOk: an error occured but it can be fixed");
+
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+
+        } else {
+            Toast.makeText(this, "can't request for map", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+}
